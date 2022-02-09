4039eb19c3218a7361370992618e089ad8e50bc7
Gejix <geraldjuwah@gmail.com>
2022-02-09 17:12:55 +0100

    Exodus

--- myApp.js
+++ myApp.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World");
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
-
-
-
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
